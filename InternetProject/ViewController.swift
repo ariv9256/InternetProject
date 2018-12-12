17076f8b1ffeb84a974aa5b9401f5e4a78353cf2
Angela Rivas <snowcats001@gmail.com>
2018-12-12 13:50:34 -0700

    Initial Commit

---
+++ InternetProject/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  InternetProject
+//
+//  Created by Rivas, Angela on 12/12/18.
+//  Copyright © 2018 CTEC CSP. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
